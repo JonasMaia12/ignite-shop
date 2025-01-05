b514e86eb32200c8db519543019a9fa69a9b28a1
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-01-05 15:44:23 +0000

    Add visual structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,20 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import Image from "next/image"
 
-export default MyApp
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={130} height={52} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
