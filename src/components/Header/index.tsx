59d8a3b5a887df60912053277c39893f8e891f58
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-01-07 17:30:16 +0000

    Adjus Cart button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,19 @@ import Link from "next/link"
 import logoImg from "../../assets/logo.svg"
 import { HeaderContainer } from "./styles"
 import { Cart } from "../Cart"
+import { useRouter } from "next/router"
 
 export function Header() {
+  const { pathname } = useRouter()
+
+  const showCartButton = pathname !== "/success"
+
   return (
     <HeaderContainer>
       <Link href="/">
         <Image src={logoImg.src} alt="" width={130} height={52} />
       </Link>
-      <Cart />
+      {showCartButton && <Cart />}
     </HeaderContainer>
   )
 }
